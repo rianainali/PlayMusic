23b8d2ffa71f456200f996745ed01f0b44b88641
rianainali <110808641+rianainali@users.noreply.github.com>
2024-03-05 08:59:12 +0300

    Create music.py

---
+++ music.py
@@ -0,0 +1,7 @@
+import time
+from playsound import playsound
+
+def play_hourly_sound():
+    playsound("Super-Mario-Bros.mp3")  # put ur song there
+if __name__ == "__main__":
+    play_hourly_sound()
